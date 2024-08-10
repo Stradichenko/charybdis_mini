870be2d8c4cbe60784e01f76b087e21d3c753f74
Stradichenko <sittoche@gmail.com>
2024-08-10 19:25:59 +0200

    first

--- keyboards/bastardkb/charybdis/3x6/keymaps/vendor/config.h
+++ keyboards/bastardkb/charybdis/3x6/keymaps/vendor/config.h
@@ -16,21 +16,82 @@
  */
 #pragma once
 
-#ifdef VIA_ENABLE
-/* VIA configuration. */
-#    define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#endif // VIA_ENABLE
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6464
+#define DEVICE_VER      0x0002
+#define MANUFACTURER    Stradichenko
+#define PRODUCT         Charybdis 3x6
 
-#ifndef __arm__
-/* Disable unused features. */
-#    define NO_ACTION_ONESHOT
-#endif // __arm__
+/* key matrix size */
+#define MATRIX_ROWS 8  // 4 rows * 2 sides
+#define MATRIX_COLS 6  // 6 columns
 
-/* Charybdis-specific features. */
+// wiring of each half
+#define MATRIX_ROW_PINS { D1, D0, D4, C6 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
 
-#ifdef POINTING_DEVICE_ENABLE
-// Automatically enable the pointer layer when moving the trackball.  See also:
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
-// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-#endif // POINTING_DEVICE_ENABLE
+// wiring for the second half
+#define MATRIX_ROW_PINS_RIGHT { D1, D0, D4, C6 }
+#define MATRIX_COL_PINS_RIGHT { F4, F5, F6, F7, B1, B3 }
+
+#define DIODE_DIRECTION COL2ROW
+
+/* Set RGB LED configuration */
+#define RGB_DI_PIN B2
+#ifdef RGB_DI_PIN
+    #define RGBLED_NUM 12
+    #define RGBLIGHT_HUE_STEP 8
+    #define RGBLIGHT_SAT_STEP 8
+    #define RGBLIGHT_VAL_STEP 8
+    #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+    #define RGBLIGHT_SLEEP  // If defined, the RGB lighting will be switched off when the host goes to sleep
+    #define RGBLIGHT_ANIMATIONS
+#endif
+
+/* Enable mouse keys and encoder support */
+#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_TIME_TO_MAX    10
+#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_WHEEL_DELAY    0
+
+#define ENCODERS_PAD_A { B4 }
+#define ENCODERS_PAD_B { B5 }
+#define ENCODER_RESOLUTION 4
+
+/* Set the debounce time to 5 ms */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* Set the tapping term (default: 200ms) */
+#define TAPPING_TERM 200
+
+/* Set to 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Prevent stuck modifiers */
+#define PREVENT_STUCK_MODIFIERS
+
+/* Split keyboard specific options */
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN D2
+#define MASTER_LEFT
+
+// If you have underglow LEDs on both halves, use the following to synchronize them:
+#define RGBLIGHT_SPLIT
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+
+/* EEPROM usage */
+#define EECONFIG_LEGACY_KEYMAP
+
+/* Define NKRO (N-Key Rollover) by default */
+#define FORCE_NKRO
+
--- keyboards/bastardkb/charybdis/3x6/keymaps/vendor/rules.mk
+++ keyboards/bastardkb/charybdis/3x6/keymaps/vendor/rules.mk
@@ -1,1 +1,63 @@
 VIA_ENABLE = yes
+# MCU configuration
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Set the default keyboard layout
+DEFAULT_FOLDER = keymaps/default
+
+# Optimize firmware size
+OPT_DEFS += -Os
+
+# Build options
+# Comment out lines to disable the feature, or set to `yes` to enable it
+RGBLIGHT_ENABLE = yes        # Enable RGB underglow lighting
+RGB_MATRIX_ENABLE = no       # Disable RGB matrix lighting (per-key RGB)
+MOUSEKEY_ENABLE = yes        # Enable mouse keys
+EXTRAKEY_ENABLE = yes        # Enable extra keys (like media controls)
+CONSOLE_ENABLE = no          # Disable the debug console
+COMMAND_ENABLE = no          # Disable command feature
+NKRO_ENABLE = yes            # Enable N-Key Rollover
+AUDIO_ENABLE = no            # Disable audio support
+MIDI_ENABLE = no             # Disable MIDI support
+UNICODE_ENABLE = no          # Disable Unicode support
+BLUETOOTH_ENABLE = no        # Disable Bluetooth support
+SLEEP_LED_ENABLE = yes        # Disable sleep LED functionality
+ENCODER_ENABLE = yes         # Enable rotary encoder support
+
+# Feature options
+TAP_DANCE_ENABLE = yes       # Enable Tap Dance feature
+LEADER_ENABLE = no           # Disable Leader key functionality
+STENO_ENABLE = no            # Disable stenography support
+BOOTMAGIC_ENABLE = no        # Disable Bootmagic (useful for debug)
+MAGIC_ENABLE = no            # Disable Magic key options (debug)
+MOUSEKEY_ENABLE = yes        # Enable mouse keys
+SPLIT_KEYBOARD = yes         # Enable split keyboard support
+USE_SERIAL = yes             # Use serial communication for split keyboard
+
+# Space optimization
+LTO_ENABLE = yes             # Enable Link Time Optimization for reducing firmware size
+
+# Set firmware target size
+FIRMWARE_SIZE = 28672
+
+# Optimization flags
+OPT_DEFS += -flto
+OPT_DEFS += -mmcu=$(MCU)
+
+# Compiler flags for split keyboard
+CFLAGS += -DSERIAL_USE_MULTI_TRANSACTION
+
+# Enable custom features
+CUSTOM_MATRIX = no           # Disable custom matrix
+CUSTOM_DEBOUNCE = no         # Disable custom debounce routine
+
+# Disable some unused features to save space
+DISABLE_PRINT = yes          # Disable print and print matrix
+DISABLE_DEBUG = yes          # Disable debug print statements
+DISABLE_ACTION_LAYER = no    # Keep layer switching functionality
+DISABLE_ACTION_TAPPING = no  # Keep tap and hold functionality
+DISABLE_ACTION_ONESHOT = no  # Keep one-shot modifiers
+
